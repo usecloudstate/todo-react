8c49845506ee633fa7f98d7d2fe852bb2fcb3bf3
Nadeesha Cabral <n@nadeesha.me>
2021-09-26 09:40:30 +1000

    Fix a11y error in original source

--- src/App.js
+++ src/App.js
@@ -116,7 +116,6 @@ function App(props) {
         {headingText}
       </h2>
       <ul
-        role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
